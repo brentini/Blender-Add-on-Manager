f60845bd62de715a0e3c3318d85558e852c0456d
Nutti <nutti.metro@gmail.com>
2016-10-21 20:03:04 +0900

    WIP: standalone db builder

---
+++ server/db.js
@@ -0,0 +1,18 @@
+'user strict';
+
+var builder = require('bl_add-on_db');
+var fs = require('fs');
+var ADDON_DB_FILE = './db/add-on_list.db';
+var config = null;
+
+fs.readFile('config.json', 'utf8', function (err, text) {
+    console.log("Parsing configuration file ...");
+    config = JSON.parse(text);
+    console.log("Parsed configuration file ...");
+    builder.init(config);
+    builder.updateDBFile(ADDON_DB_FILE);
+});
+
+
+/*while (1) {
+}*/
