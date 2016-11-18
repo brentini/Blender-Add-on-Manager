941ff4f6818804b805f3023ab0d0b7efc8518705
Nutti <nutti.metro@gmail.com>
2016-11-18 23:50:56 +0900

    WIP: Switch IF between GitHub and Installed Add-ons

--- src/js/db.js
+++ src/js/db.js
@@ -3,8 +3,16 @@
 var fs = require('fs');
 var builder = require('bl_add-on_db');
 var path = require('path');
-var ADDON_DB_FILE = path.resolve('./db/add-on_list.db');
-var addonDB = null;
+
+var utils = require('nutti_utils');
+
+
+var GITHUB_ADDONS_DB = path.resolve('./db/add-on_list.db');
+var INSTALLED_ADDONS_DB = path.resolve('./db/installed_add-on_list.db');
+
+var githubAddons = null;
+var installedAddons = null;
+
 var config = null;
 
 var app = angular.module('readus', [])
@@ -15,20 +23,37 @@ app.controller('MainController', function ($scope, $timeout) {
 
     main.repoList = [];
     $timeout(function() {
-        main.repoList = addonDB;
+        main.repoList = githubAddons;
+    });
+    $('#btn-github-addonlist').click(function (e) {
+        console.log("Show GitHub add-on list");
+        main.repoList = githubAddons;
+    });
+    $('#btn-installed-addonlist').click(function (e) {
+        console.log("Show Installed add-on list");
+        main.repoList = installedAddons['2.77'];
     });
 });
 
 
+
 $('#update-db').click(function (e) {
-    builder.updateDBFile(ADDON_DB_FILE);
+    builder.updateDBFile(GITHUB_ADDONS_DB);
 });
 
-if (builder.isExistFile(ADDON_DB_FILE)) {
-    console.log("Reading add-on DB file ...")
-    addonDB = builder.readDBFile(ADDON_DB_FILE);
-    console.log(addonDB);
+if (utils.isExistFile(GITHUB_ADDONS_DB)) {
+    console.log("Reading GitHub add-ons DB file ...")
+    githubAddons = builder.readDBFile(GITHUB_ADDONS_DB);
+    console.log(githubAddons);
 }
+
+if (utils.isExistFile(INSTALLED_ADDONS_DB)) {
+    console.log("Reading installed add-ons DB file ...");
+    installedAddons = builder.readDBFile(INSTALLED_ADDONS_DB);
+    console.log(installedAddons);
+}
+
+
 fs.readFile('config.json', 'utf8', function (err, text) {
     console.log("Parsing configuration file ...");
     config = JSON.parse(text);
