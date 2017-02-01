47652e1d476a4c6d9c3682c5bc75cf992e28eec0
Nutti <nutti.metro@gmail.com>
2017-02-01 22:51:05 +0900

    Update: parse bl_info algorithm

--- server/db.js
+++ server/db.js
@@ -6,13 +6,17 @@ var path = require('path');
 var ADDON_DB_FILE = path.resolve('./db/add-on_list.db');
 var config = null;
 
-console.log(ADDON_DB_FILE);
+if (process.argv.length != 4) {
+    console.log("argument error\nUsage: node db.js start_page end_page");
+    process.exit(1);
+}
 
 fs.readFile('config.json', 'utf8', function (err, text) {
     console.log("Parsing configuration file ...");
     config = JSON.parse(text);
     console.log("Parsed configuration file ...");
     builder.init(config);
+    builder.setPage(+process.argv[2], +process.argv[3]);
     builder.updateDBFile(ADDON_DB_FILE);
 });
 
