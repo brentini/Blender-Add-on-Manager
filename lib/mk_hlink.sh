32988ee10d5897be1f57c5b7dcad0e9415029900
Nutti <nutti.metro@gmail.com>
2017-02-08 21:55:49 +0900

    Add: option

--- lib/mk_hlink.sh
+++ lib/mk_hlink.sh
@@ -6,6 +6,7 @@ for file in `\find . -maxdepth 1 -type f`
 do
     src=${file}
     target=${TARGET_DIR}/${file}
+    rm -rf ${target}
     ln ${src} ${target}
     echo ${src}" -> "${target}
 done
--- server/db.js
+++ server/db.js
@@ -3,20 +3,58 @@
 var builder = require('bl_add-on_db');
 var fs = require('fs');
 var path = require('path');
+var opts = require('opts');
+
 var ADDON_DB_FILE = path.resolve('./db/add-on_list.db');
 var config = null;
 
-if (process.argv.length != 6) {
-    console.log("argument error\nUsage: node db.js start_page end_page");
-    process.exit(1);
-}
+var startPage = 0;
+var endPage = 1;
+var minFileSize = 0;
+var maxFileSize = 1000;
+
+var options = [
+    {
+        'short': 'p',
+        'long': 'page',
+        'description': 'Page',
+        'value': true,
+        'required': false,
+        'callback': (value) => {
+            var page = value.split('-');
+            if (page.length != 2) { console.log("page"); process.exit(1); }
+            if (page[0] == '' || page[0].match(/\D/)) { console.log("pnum0"); process.exit(1); }
+            if (page[1] == '' || page[1].match(/\D/)) { console.log("pnum1"); process.exit(1); }
+            startPage = +page[0];
+            endPage = +page[1];
+        }
+    },
+    {
+        'short': 's',
+        'long': 'filesize',
+        'description': 'Size of source code',
+        'value': true,
+        'required': false,
+        'callback': (value) => {
+            var size = value.split('-');
+            if (size.length != 2) { console.log("size"); process.exit(1); }
+            if (size[0] == '' || size[0].match(/\D/)) { console.log("pnum0"); process.exit(1); }
+            if (size[1] == '' || size[1].match(/\D/)) { console.log("pnum1"); process.exit(1); }
+            minFileSize = +size[0];
+            maxFileSize = +size[1];
+        }
+    },
+];
+
+opts.parse(options);
+
 
 fs.readFile('config.json', 'utf8', function (err, text) {
     console.log("Parsing configuration file ...");
     config = JSON.parse(text);
     console.log("Parsed configuration file ...");
     builder.init(config);
-    builder.setPage(+process.argv[2], +process.argv[3], +process.argv[4], +process.argv[5]);
+    builder.setQueryOpts(startPage, endPage, minFileSize, maxFileSize);
     builder.updateDBFile(ADDON_DB_FILE);
 });
 
